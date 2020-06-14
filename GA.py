976597ef7e1a77cdc79646d9d18d09a29bfd3f83
MatheusF <matheussanches531@gmail.com>
2020-06-14 18:47:01 -0300

    bug da linha 216.. :-D

--- GA.py
+++ GA.py
@@ -213,7 +213,7 @@ class GeneticAlgorithm:
             for i in range(len(self.population)):
                 todos_genomes.append(self.population[i].genome.tolist())
 
-            self.historic.append({"geracao":self.iteration_counter,"max":score_geracao_max,"min":score_geracao_min,"avg":score_geracao_medio,"best":self.best_element_total.score,"best_genome":self.best_element_total.genome.tolist(),"todos_genomes":todos_genomes})toric.append({"geracao":self.iteration_counter,"max":score_geracao_max,"min":score_geracao_min,"avg":score_geracao_medio,"best":self.best_element_total.score,"best_genome":self.best_element_total.genome.tolist(),"todos_genomes":todos_genomes})
+            self.historic.append({"geracao":self.iteration_counter,"max":score_geracao_max,"min":score_geracao_min,"avg":score_geracao_medio,"best":self.best_element_total.score,"best_genome":self.best_element_total.genome.tolist(),"todos_genomes":todos_genomes})
 
 
     """
