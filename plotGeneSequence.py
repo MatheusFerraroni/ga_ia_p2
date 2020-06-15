6c37dd085a68ba145e8be2eebea57c931a7f2dcd
abe Lincol <aissa.hadj.mohamed@gmail.com>
2020-06-15 15:28:11 -0300

    corrected bug to remove target

--- plotGeneSequence.py
+++ plotGeneSequence.py
@@ -2,17 +2,18 @@ import seaborn as sns; sns.set()
 import matplotlib.pyplot as plt
 import pandas as pd
 
-def returnLabelsForCSVFile(csvFilePath):
+def returnLabelsForCSVFile(csvFilePath, targetName):
     featureLabels = []
     file = pd.read_csv(csvFilePath)
     featureLabels = list(file.head(0))
+    featureLabels.remove(targetName)
     return featureLabels
 
-def plotBestGenome(resultDatasetPath, datasetPath, figSizeArray = [10, 8]):
+def plotBestGenome(resultDatasetPath, datasetPath, targetName,figSizeArray = [10, 8]):
 
     dataset = openFile(resultDatasetPath)
     bestGenomePerGenerationArray = []
-    featureLabels = returnLabelsForCSVFile(datasetPath)
+    featureLabels = returnLabelsForCSVFile(datasetPath, targetName)
 
     for historicElem in dataset['historic']:
         bestGenomePerGenerationArray.append(historicElem['best_genome'])
